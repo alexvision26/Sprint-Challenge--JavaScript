39c709efed5826bedbbeda65f9a7291188373751
Alex Miller <alexmillerprojects@gmail.com>
2019-12-20 09:45:40 -0800

    finished main project, working on stretch

--- challenges/objects-arrays.js
+++ challenges/objects-arrays.js
@@ -100,7 +100,11 @@ console.log(contactInfo);
 /* Request 3: Find out how many universities have the string "Uni" included in their name. Create a new array called unisWithUni that contains them all. This will be an array of objects. Log the result. */
 const unisWithUni = [];
 
-
+graduates.filter(function(item){
+  if (item.university.includes('Uni') === true){
+    unisWithUni.push(item.university)
+  }
+})
 
 console.log(unisWithUni);
 
