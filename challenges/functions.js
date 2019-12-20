4724481edd66b5367690c9ba78df5bf2f6df3a79
Alex Miller <alexmillerprojects@gmail.com>
2019-12-20 09:25:23 -0800

    finished with entire project except one question...

--- challenges/functions.js
+++ challenges/functions.js
@@ -7,6 +7,10 @@
   * The consume function should return the invocation of cb, passing a and b into cb as arguments
 */
 
+function consume (a, b, cb){
+  return cb(a, b);
+}
+
 
 /* Step 2: Create several functions to callback with consume();
   * Create a function named add that returns the sum of two numbers
@@ -14,18 +18,30 @@
   * Create a function named greeting that accepts a first and last name and returns "Hello first-name last-name, nice to meet you!"
 */
 
+function add(num1, num2){
+  return num1 + num2;
+}
+
+function multiply(num1, num2){
+  return num1 * num2;
+}
+
+function greeting(first, last){
+  return `Hello ${first} ${last}, nice to meet you!`
+}
+
 
 /* Step 3: Check your work by un-commenting the following calls to consume(): */
-// console.log(consume(2, 2, add)); // 4
-// console.log(consume(10, 16, multiply)); // 160
-// console.log(consume("Mary", "Poppins", greeting)); // Hello Mary Poppins, nice to meet you!
+console.log(consume(2, 2, add)); // 4
+console.log(consume(10, 16, multiply)); // 160
+console.log(consume("Mary", "Poppins", greeting)); // Hello Mary Poppins, nice to meet you!
 
 
 // ==== Closures ==== 
 
 // Explain in your own words why nestedfunction can access the variable internal.
 
-// Explanation: 
+// Explanation: The reason nestedFunction can access the variable, 'internal' is because it is in the parent of the function. It is also outside the scope of function and function can look outside the their scope to access variables. When data or a variable is inside another function that is outside of it's scope, the function cannot access the variable. In this case, the nestedFunction is still within myFunction so it can access it. But if nestedFunction was not within myFunction, it would not have access to the variable, 'internal'.
 
 
 const external = "I'm outside the function";
--- challenges/objects-arrays.js
+++ challenges/objects-arrays.js
@@ -99,6 +99,9 @@ console.log(contactInfo);
 
 /* Request 3: Find out how many universities have the string "Uni" included in their name. Create a new array called unisWithUni that contains them all. This will be an array of objects. Log the result. */
 const unisWithUni = [];
+
+
+
 console.log(unisWithUni);
 
 
@@ -163,7 +166,10 @@ console.log(lowPopulationAnimals);
 The zoos need to know their total animal population across the United States. Find the total population from all the zoos using the .reduce() method. Remember the reduce method takes two arguments: a callback (which itself takes two args), and an initial value for the count.
 
 */
-const populationTotal = 0;
+
+const populationTotal = zooAnimals.reduce(function(acc,cur){
+  return acc + cur.population;
+}, 0)
 
 console.log(populationTotal);
 
